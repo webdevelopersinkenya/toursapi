a029ae0dc21ee89e1db9cc99210c03a394ea15d0
webdevelopersinkenya <mirr22006@byui.edu>
2026-06-09 13:39:00 +0300

    changes

--- models/destinationModel.js
+++ models/destinationModel.js
@@ -1,9 +1,30 @@
 const mongoose = require("mongoose");
 
-const destinationSchema = new mongoose.Schema({
-  name: String,
-  country: String,
-  description: String
-});
+const destinationSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Destination name is required"],
+      trim: true,
+      minlength: 2
+    },
+
+    country: {
+      type: String,
+      required: [true, "Country is required"],
+      trim: true
+    },
+
+    description: {
+      type: String,
+      required: [true, "Description is required"],
+      trim: true,
+      minlength: 10
+    }
+  },
+  {
+    timestamps: true
+  }
+);
 
 module.exports = mongoose.model("Destination", destinationSchema);
--- models/tourModel.js
+++ models/tourModel.js
@@ -1,18 +1,29 @@
 const mongoose = require("mongoose");
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Tour name is required"],
+      trim: true,
+      minlength: 3
+    },
+
+    destination: {
+      type: String,
+      required: [true, "Destination is required"],
+      trim: true
+    },
+
+    price: {
+      type: Number,
+      required: [true, "Price is required"],
+      min: [0, "Price cannot be negative"]
+    }
   },
-  destination: {
-    type: String,
-    required: true
-  },
-  price: {
-    type: Number,
-    required: true
+  {
+    timestamps: true
   }
-});
+);
 
 module.exports = mongoose.model("Tour", tourSchema);

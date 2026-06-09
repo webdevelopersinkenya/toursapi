388879fe84fc13ae7a2c56ab63cbca028a8fa713
webdevelopersinkenya <mirr22006@byui.edu>
2026-06-09 16:12:12 +0300

    changes

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,30 +2,28 @@ const jwt = require("jsonwebtoken");
 
 /**
  * AUTH MIDDLEWARE (PROTECT ROUTES)
- * Checks if user has a valid JWT token
  */
 const protect = (req, res, next) => {
   try {
+    console.log("JWT_SECRET:", process.env.JWT_SECRET);
+    console.log("AUTH HEADER:", req.headers.authorization);
+
     let token;
 
-    // Check Authorization header
     const authHeader = req.headers.authorization;
 
     if (authHeader && authHeader.startsWith("Bearer ")) {
       token = authHeader.split(" ")[1];
     }
 
-    // No token found
     if (!token) {
       return res.status(401).json({
         message: "Not authorized, token missing"
       });
     }
 
-    // Verify token
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    // Attach user info to request
     req.user = decoded;
 
     next();
